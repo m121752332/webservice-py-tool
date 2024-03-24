93d8d4881525613d73ad51dc38f14dde6e0f7a27
Game <m121752332@gmail.com>
2024-03-24 21:52:14 +0800

    增加選單並提供快捷鍵操作 F1:新增 F2:刪除 F3:讀取 F5:執行請求 F6:清空服務

--- src/ui/main/main_frame.py
+++ src/ui/main/main_frame.py
@@ -11,7 +11,12 @@ import wx
 import wx.xrc
 
 ID_ESC = 1000
-ID_ABOUT = 1001
+ID_NEW = 1001
+ID_DEL = 1002
+ID_LOAD = 1003
+ID_RUN = 1004
+ID_CLEAR = 1005
+ID_ABOUT = 1006
 
 ###########################################################################
 ## Class MainFrame
@@ -24,6 +29,41 @@ class MainFrame ( wx.Frame ):
 
 		self.SetSizeHints( wx.Size( 800,550 ), wx.Size( -1,-1 ) )
 		self.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
+
+		self.m_menubar = wx.MenuBar( 0|wx.BORDER_THEME )
+		self.m_file = wx.Menu()
+		self.m_item_exit = wx.MenuItem( self.m_file, ID_ESC, u"離開"+ u"\t" + u"esc", u"按下Esc離開", wx.ITEM_NORMAL )
+		self.m_file.Append( self.m_item_exit )
+
+		self.m_menubar.Append( self.m_file, u"檔案" )
+
+		self.m_run = wx.Menu()
+		self.m_item_new = wx.MenuItem( self.m_run, ID_NEW, u"新增配置"+ u"\t" + u"f1", u"新增連線配置[F1]", wx.ITEM_NORMAL )
+		self.m_run.Append( self.m_item_new )
+
+		self.m_item_del = wx.MenuItem( self.m_run, ID_DEL, u"刪除配置"+ u"\t" + u"f2", u"刪除連線配置[F2]", wx.ITEM_NORMAL )
+		self.m_run.Append( self.m_item_del )
+
+		self.m_item_load = wx.MenuItem( self.m_run, ID_LOAD, u"讀取操作"+ u"\t" + u"f3", u"讀取服務網址的服務[F3]", wx.ITEM_NORMAL )
+		self.m_run.Append( self.m_item_load )
+
+		self.m_run.AppendSeparator()
+
+		self.m_item_run = wx.MenuItem( self.m_run, ID_RUN, u"執行請求"+ u"\t" + u"f5", u"執行貼上的請求內容[F5]", wx.ITEM_NORMAL )
+		self.m_run.Append( self.m_item_run )
+
+		self.m_item_clear = wx.MenuItem( self.m_run, ID_CLEAR, u"清空服務格內容"+ u"\t" + u"f6", u"清除服務的內容，如操作服務+請求+回應[F6]", wx.ITEM_NORMAL )
+		self.m_run.Append( self.m_item_clear )
+
+		self.m_menubar.Append( self.m_run, u"執行" )
+
+		self.m_help = wx.Menu()
+		self.m_item_about = wx.MenuItem( self.m_help, ID_ABOUT, u"About"+ u"\t" + u"f8", u"作者資訊", wx.ITEM_NORMAL )
+		self.m_help.Append( self.m_item_about )
+
+		self.m_menubar.Append( self.m_help, u"幫助" )
+
+		self.SetMenuBar( self.m_menubar )
 
 		bSizerMain = wx.BoxSizer( wx.VERTICAL )
 
@@ -77,7 +117,7 @@ class MainFrame ( wx.Frame ):
 		bSizerHelp = wx.BoxSizer( wx.HORIZONTAL )
 
 		bSizerHelp.SetMinSize( wx.Size( 0,0 ) )
-		self.m_static_text_dummy = wx.StaticText( sbSizerTop.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 100,-1 ), 0 )
+		self.m_static_text_dummy = wx.StaticText( sbSizerTop.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 80,-1 ), 0 )
 		self.m_static_text_dummy.Wrap( -1 )
 
 		bSizerHelp.Add( self.m_static_text_dummy, 0, wx.ALL, 1 )
@@ -126,53 +166,51 @@ class MainFrame ( wx.Frame ):
 
 		bSizerMain.Add( sbSizerTop, 0, wx.ALL|wx.EXPAND, 5 )
 
-		bSizerBottom = wx.BoxSizer( wx.HORIZONTAL )
+		self.m_splitter_method = wx.SplitterWindow( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.SP_3D )
+		self.m_splitter_method.Bind( wx.EVT_IDLE, self.m_splitter_methodOnIdle )
+		self.m_splitter_method.SetMinimumPaneSize( 50 )
 
-		sbSizerLeft = wx.StaticBoxSizer( wx.StaticBox( self, wx.ID_ANY, u"參數（多個用#~#隔開）" ), wx.VERTICAL )
+		self.m_panel_left = wx.Panel( self.m_splitter_method, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		sbSizerLeft = wx.StaticBoxSizer( wx.StaticBox( self.m_panel_left, wx.ID_ANY, u"請求參數（多個用#~#隔開）" ), wx.VERTICAL )
 
 		self.m_text_ctrl_params = wx.TextCtrl( sbSizerLeft.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.TE_MULTILINE|wx.TE_WORDWRAP )
 		sbSizerLeft.Add( self.m_text_ctrl_params, 1, wx.ALL|wx.EXPAND, 5 )
 
 
-		bSizerBottom.Add( sbSizerLeft, 1, wx.ALL|wx.EXPAND, 5 )
-
-		sbSizerRight = wx.StaticBoxSizer( wx.StaticBox( self, wx.ID_ANY, u"回應結果" ), wx.VERTICAL )
+		self.m_panel_left.SetSizer( sbSizerLeft )
+		self.m_panel_left.Layout()
+		sbSizerLeft.Fit( self.m_panel_left )
+		self.m_panel_right = wx.Panel( self.m_splitter_method, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		sbSizerRight = wx.StaticBoxSizer( wx.StaticBox( self.m_panel_right, wx.ID_ANY, u"回應結果" ), wx.VERTICAL )
 
 		self.m_text_ctrl_result = wx.TextCtrl( sbSizerRight.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.TE_MULTILINE|wx.TE_READONLY|wx.TE_WORDWRAP )
 		sbSizerRight.Add( self.m_text_ctrl_result, 1, wx.ALL|wx.EXPAND, 5 )
 
 
-		bSizerBottom.Add( sbSizerRight, 1, wx.ALL|wx.EXPAND, 5 )
-
-
-		bSizerMain.Add( bSizerBottom, 1, wx.EXPAND, 5 )
+		self.m_panel_right.SetSizer( sbSizerRight )
+		self.m_panel_right.Layout()
+		sbSizerRight.Fit( self.m_panel_right )
+		self.m_splitter_method.SplitVertically( self.m_panel_left, self.m_panel_right, 324 )
+		bSizerMain.Add( self.m_splitter_method, 1, wx.EXPAND|wx.RESERVE_SPACE_EVEN_IF_HIDDEN, 5 )
 
 
 		self.SetSizer( bSizerMain )
 		self.Layout()
 		self.m_status = self.CreateStatusBar( 1, wx.STB_SIZEGRIP, wx.ID_ANY )
 		self.m_status.SetToolTip( u"請選擇網址再選擇服務方法" )
-
-		self.m_menubar = wx.MenuBar( wx.MB_DOCKABLE )
-		self.file = wx.Menu()
-		self.exit = wx.MenuItem( self.file, ID_ESC, u"離開"+ u"\t" + u"esc", u"按下Esc離開", wx.ITEM_NORMAL )
-		self.file.Append( self.exit )
-
-		self.m_menubar.Append( self.file, u"檔案" )
-
-		self.help = wx.Menu()
-		self.about = wx.MenuItem( self.help, ID_ABOUT, u"About"+ u"\t" + u"f8", u"作者資訊", wx.ITEM_NORMAL )
-		self.help.Append( self.about )
-
-		self.m_menubar.Append( self.help, u"幫助" )
-
-		self.SetMenuBar( self.m_menubar )
+		self.m_status.SetHelpText( u"請選擇網址再選擇服務方法" )
 
 
 		self.Centre( wx.HORIZONTAL )
 
 		# Connect Events
 		self.Bind( wx.EVT_CLOSE, self.OnMenuClickEventExit )
+		self.Bind( wx.EVT_MENU, self.OnMenuClickEventExit, id = self.m_item_exit.GetId() )
+		self.Bind( wx.EVT_MENU, self.OnClickEventDel, id = self.m_item_del.GetId() )
+		self.Bind( wx.EVT_MENU, self.OnClickEventLoad, id = self.m_item_load.GetId() )
+		self.Bind( wx.EVT_MENU, self.OnClickEventStart, id = self.m_item_run.GetId() )
+		self.Bind( wx.EVT_MENU, self.OnClickEventClear, id = self.m_item_clear.GetId() )
+		self.Bind( wx.EVT_MENU, self.OnMenuClickEventAbout, id = self.m_item_about.GetId() )
 		self.m_text_ctrl_name.Bind( wx.EVT_KILL_FOCUS, self.OnTextCtrlNameKillFocus )
 		self.m_text_ctrl_name.Bind( wx.EVT_SET_FOCUS, self.OnTextCtrlNameSetFocus )
 		self.m_text_ctrl_name.Bind( wx.EVT_TEXT, self.OnTextCtrlNameText )
@@ -189,8 +227,6 @@ class MainFrame ( wx.Frame ):
 		self.m_combo_methods.Bind( wx.EVT_TEXT_ENTER, self.OnComboBoxMethodTextEnter )
 		self.m_btn_start.Bind( wx.EVT_BUTTON, self.OnClickEventStart )
 		self.m_btn_clear.Bind( wx.EVT_BUTTON, self.OnClickEventClear )
-		self.Bind( wx.EVT_MENU, self.OnMenuClickEventExit, id = self.exit.GetId() )
-		self.Bind( wx.EVT_MENU, self.OnMenuClickEventAbout, id = self.about.GetId() )
 
 	def __del__( self ):
 		pass
@@ -198,6 +234,22 @@ class MainFrame ( wx.Frame ):
 
 	# Virtual event handlers, override them in your derived class
 	def OnMenuClickEventExit( self, event ):
+		event.Skip()
+
+
+	def OnClickEventDel( self, event ):
+		event.Skip()
+
+	def OnClickEventLoad( self, event ):
+		event.Skip()
+
+	def OnClickEventStart( self, event ):
+		event.Skip()
+
+	def OnClickEventClear( self, event ):
+		event.Skip()
+
+	def OnMenuClickEventAbout( self, event ):
 		event.Skip()
 
 	def OnTextCtrlNameKillFocus( self, event ):
@@ -218,8 +270,6 @@ class MainFrame ( wx.Frame ):
 	def OnClickEventAdd( self, event ):
 		event.Skip()
 
-	def OnClickEventDel( self, event ):
-		event.Skip()
 
 	def OnComboBoxUrlsSelect( self, event ):
 		event.Skip()
@@ -230,8 +280,6 @@ class MainFrame ( wx.Frame ):
 	def OnComboBoxUrlsEnter( self, event ):
 		event.Skip()
 
-	def OnClickEventLoad( self, event ):
-		event.Skip()
 
 	def OnComboBoxMethodSelect( self, event ):
 		event.Skip()
@@ -242,14 +290,10 @@ class MainFrame ( wx.Frame ):
 	def OnComboBoxMethodTextEnter( self, event ):
 		event.Skip()
 
-	def OnClickEventStart( self, event ):
-		event.Skip()
-
-	def OnClickEventClear( self, event ):
-		event.Skip()
 
 
-	def OnMenuClickEventAbout( self, event ):
-		event.Skip()
+	def m_splitter_methodOnIdle( self, event ):
+		self.m_splitter_method.SetSashPosition( 324 )
+		self.m_splitter_method.Unbind( wx.EVT_IDLE )
 
 
